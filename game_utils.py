a2bcb9010d91be70ed7572751a003098ae67b22e
chris-conklin <cconklin2376@gmail.com>
2017-09-20 21:09:57 -0400

    Isolated get_game_type menu to game utils

--- game_utils.py
+++ game_utils.py
@@ -4,7 +4,12 @@ from stock import Stock
 class GameUtils(object):
     @staticmethod
     def main_menu():
-        menu_string = ''' What would you like to do?
-        (C)ontinue
-        (Q)uit'''
-        return raw_input(menu_string)
+        menu_string = '''What would you like to do?\n(C)ontinue\n(Q)uit\n? '''
+        return raw_input(menu_string).upper()
+
+    @staticmethod
+    def get_game_type():
+        menu_string = "Is this a (N)ew or (E)xisting game\n?"
+        return raw_input(menu_string).upper()
+    
+        
