421b72b9de019337ecceb0aebde44c3d63a0b470
spiffycode23 <spiffycode23@gmail.com>
2017-09-20 20:35:57 -0400

    Added new_game and load_game stubs

--- global_market.py
+++ global_market.py
@@ -11,7 +11,20 @@ class GlobalMarket(object):
         self.optimism = 1.0
         self.rountcnt = 0
 
+    def prepare(self):
+        # is this a new or existing game?
+        choice = raw_input("Is this a (N)ew or (E)xisting game?")
+        if ''.upper(choice) == 'N':
+            self.new_game()
+        else:
+            self.load_game()
     
+    def new_game(self):
+        print "We're getting kinda rabbit holed!"
+
+    def load_game(self):
+        print "We're not there yet"
+
     def get_global_market_value(self):
         '''return the total net worth of stocks,funds and comm'''
         total = 0
