cb80fb6e837aecfa6c25c2234813911895ff380f
chris-conklin <cconklin2376@gmail.com>
2017-09-20 21:16:06 -0400

    validation to main_menu inputs

--- game_utils.py
+++ game_utils.py
@@ -4,12 +4,19 @@ from stock import Stock
 class GameUtils(object):
     @staticmethod
     def main_menu():
+        valid_inputs = ['C', 'c', 'Q','q']
         menu_string = '''What would you like to do?\n(C)ontinue\n(Q)uit\n? '''
-        return raw_input(menu_string).upper()
+        k = raw_input(menu_string).upper()
+        while k not in valid_inputs:
+            print
+            print ">>> Invalid Selection!"
+            print
+            k = raw_input(menu_string).upper()
+        return k
 
     @staticmethod
     def get_game_type():
-        menu_string = "Is this a (N)ew or (E)xisting game\n?"
+        menu_string = '''Is this a (N)ew or (E)xisting game\n?'''
         return raw_input(menu_string).upper()
-    
+
         
